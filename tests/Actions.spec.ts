1019e53d2cf3d22792db5fc532006d8e97507d1f
syed mazhar <94785463+TestGitHubMazhar@users.noreply.github.com>
2026-05-20 18:09:41 +0530

    Delete tests directory

--- tests/Actions.spec.ts
+++
@@ -1,7 +0,0 @@
-import {test,expect} from "@playwright/test"
-test('actions',async({page})=>{
-    await page.goto('https://testautomationpractice.blogspot.com');
-    await page.getByRole('textbox',{name:'name'}).fill('enter nae');
-    await page.waitForTimeout(5000);
-    await expect(page).toHaveTitle('Automation Testing Practice')
-})
--- tests/function.js
+++
@@ -1,8 +0,0 @@
-exports.Hello=function f1()
-{
-    return 'Hello'
-}
-exports.HelloWord=function f2()
-{
-    return 'Hello world'
-}
